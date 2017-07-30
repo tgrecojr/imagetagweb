a6d86c5063ef2a9e48ea354702ce8cebed87c329
T.J. Greco <tgrecojr@gmail.com>
2017-07-30 07:13:46 -0400

    first commit of web application skeleton

---
+++ src/main/java/com/greco/imagetag/ImagetagwebApplication.java
@@ -0,0 +1,12 @@
+package com.greco.imagetag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagetagwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagetagwebApplication.class, args);
+	}
+}
